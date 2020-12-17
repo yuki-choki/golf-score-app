5b7fabba41e3bb06157af512b5156e9641235eff
yuki-choki <ikuy0105@gmail.com>
2020-12-18 07:28:31 +0900

    LoginController作成
    ・ログインアクション作成
    ・ログインページ作成
    ・サインアップアクション作成
    ・サインアップページ作成
    ・rootアクセスページをログインページに変更

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('login.index');
+    }
+
+    public function signup(Request $request)
+    {
+        return view('login.signup');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'LoginController@index');
+Route::get('login', 'LoginController@index');
+Route::get('login/signup', 'LoginController@signup');
