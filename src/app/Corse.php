4e52a4807af7defb5dd9fe392c8131aac0cf32f8
yuki-choki <ikuy0105@gmail.com>
2021-01-17 13:54:27 +0900

    Modelファイルの修正
    ・リレーション追加
    ・グローバルスコープ追加（Gameのみ）
    ・カラムの制約を$fillable → $guardedに変更

--- src/app/Corse.php
+++ src/app/Corse.php
@@ -8,4 +8,21 @@ class Corse extends Model
 {
     // 意図的に更新しないカラムを指定
     protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function scopeSearchCorse($query, $array)
+    {
+        if ($array['pref_code'] !== '0') {
+            $query->where('pref_code', $array['pref_code']);
+        }
+        if ($array['name']) {
+            $query->where('name', 'like', '%'. $array['name'] .'%');
+        }
+
+        return $query;
+    }
+
+    public function games()
+    {
+        return $this->hasMany(Game::class);
+    }
 }
--- src/app/Game.php
+++ src/app/Game.php
@@ -2,11 +2,29 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Game extends Model
 {
     // 意図的に更新しないカラムを指定
     protected $guarded = ['id', 'created_at', 'updated_at'];
 
+    protected static function booted()
+    {
+        static::addGlobalScope('user', function (Builder $builder) {
+            $builder->where('user_id', Auth::id());
+        });
+    }
+
+    public function corse()
+    {
+        return $this->belongsTo(Corse::class);
+    }
+
+    public function score_cards()
+    {
+        return $this->hasMany(ScoreCard::class);
+    }
 }
