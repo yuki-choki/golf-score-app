d01f02949658c62577fb1d9f4ae99b6b9b89b36d
yuki-choki <ikuy0105@gmail.com>
2021-02-10 22:20:18 +0900

    サイドバーのマウスオーバー時BG変更
    濃い色に変更

--- src/resources/views/components/user/sidebar.blade.php
+++ src/resources/views/components/user/sidebar.blade.php
@@ -1,30 +1,30 @@
 <div class="col-md-2 side-bar p-0">
     <a class="hover:no-underline" href="{{ route('home') }}">
-        <div class="side-bar-item d-flex p-3 hover:bg-base-dark hover:text-white">
+        <div class="side-bar-item d-flex p-3 hover:bg-base-deep hover:text-white">
             <div class="col-2 text-center p-0"><i class="fas fa-home fa-lg leading-6"></i></div>
             <div class="col-10 ml-3 p-0"><span class="text-base">ホーム</span></div>
         </div>
     </a>
     <a class="hover:no-underline" href="{{ route('users.edit') }}">
-        <div class="side-bar-item d-flex p-3 hover:bg-base-dark hover:text-white">
+        <div class="side-bar-item d-flex p-3 hover:bg-base-deep hover:text-white">
             <div class="col-2 text-center p-0"><i class="fas fa-user-edit fa-lg leading-6"></i></div>
             <div class="col-10 ml-3 p-0"><span class="text-base">プロフィール編集</span></div>
         </div>
     </a>
     <a class="hover:no-underline" href="{{ route('password.form') }}">
-        <div class="side-bar-item d-flex p-3 hover:bg-base-dark hover:text-white">
+        <div class="side-bar-item d-flex p-3 hover:bg-base-deep hover:text-white">
             <div class="col-2 text-center p-0"><i class="fas fa-lock fa-lg leading-6"></i></div>
             <div class="col-10 ml-3 p-0"><span class="text-base">パスワード変更</span></div>
         </div>
     </a>
     <a class="hover:no-underline" href="#">
-        <div class="side-bar-item d-flex p-3 hover:bg-base-dark hover:text-white">
+        <div class="side-bar-item d-flex p-3 hover:bg-base-deep hover:text-white">
             <div class="col-2 text-center p-0"><i class="fas fa-user-friends fa-lg leading-6"></i></div>
             <div class="col-10 ml-3 p-0"><span class="text-base">友達</span></div>
         </div>
     </a>
     <a class="hover:no-underline" href="#">
-        <div class="side-bar-item d-flex p-3 hover:bg-base-dark hover:text-white">
+        <div class="side-bar-item d-flex p-3 hover:bg-base-deep hover:text-white">
             <div class="col-2 text-center p-0"><i class="fas fa-cog fa-lg leading-6"></i></div>
             <div class="col-10 ml-3 p-0"><span class="text-base">設定</span></div>
         </div>
