d3142d9244821f257131aa293e7430828d588337
yuki-choki <ikuy0105@gmail.com>
2021-02-14 21:58:11 +0900

    ユーザ関連の画面修正
    ・googleのSNS認証ボタンのpath変更
    ・確認用パスワードのバグ修正
    　→フィールド名が間違えていた
    ・validationエラーのメッセージをまとめて上部に表示するよう変更

--- src/resources/views/auth/passwords/reset.blade.php
+++ src/resources/views/auth/passwords/reset.blade.php
@@ -2,61 +2,65 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
-
-                        <input type="hidden" name="token" value="{{ $token }}">
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
+    <div class="row">
+        @include('components.user.sidebar')
+        <div class="col-md-10 pt-3 bg-gray-50">
+            <div class="pt-5 m-auto" style="width: 410px;">
+                <div class="text-center">
+                    <h3 class="h3">パスワードリセット</h3>
+                </div>
+                <div class="card">
+                    <div class="card-body"> 
+                        {{ Form::open(['url' => route('password.update')]) }}
+                            <div class="form-group">
+                                {{ Form::label('email', 'メールアドレス', ['mb-0']) }}
+                                {{ Form::text(
+                                    'email',
+                                    old('email'),
+                                    $errors->has('email')
+                                        ? ['class' => 'form-control is-invalid', 'required']
+                                        : ['class' => 'form-control', 'required']
+                                    )
+                                }}
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
+                            <div class="form-group">
+                                {{ Form::label('password', 'パスワード', ['class' => 'mb-0']) }}
+                                {{ Form::password(
+                                    'password',
+                                    $errors->has('password')
+                                        ? ['class' => 'form-control is-invalid']
+                                        : ['class' => 'form-control'])
+                                }}
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            <div class="form-group">
+                                {{ Form::label('password-confirm', 'パスワード確認用', ['class' => 'mb-0']) }}
+                                {{ Form::password(
+                                    'password-confirm',
+                                    $errors->has('password-confirm')
+                                        ? ['class' => 'form-control is-invalid', 'required']
+                                        : ['class' => 'form-control', 'required'])
+                                }}
+                                @error('password-confirm')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
+                            <div class="form-group mb-0">
+                                {{ Form::submit('更新', ['class' => 'btn btn-primary']) }}
                             </div>
-                        </div>
-                    </form>
+                        {{ Form::close() }}
+                    </div>
                 </div>
             </div>
         </div>
