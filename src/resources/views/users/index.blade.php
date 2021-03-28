a7566f7144254a8030092f0957ec4429767fdc4c
yuki-choki <ikuy0105@gmail.com>
2021-03-28 18:11:25 +0900

    ユーザ情報表示画面の修正（画像が表示されない）
    ・ユーザのプロフィール画像が設定されていない場合に未設定用の画像が表示されない
    　→未設定の場合に「no-user-image」を表示するように修正

--- src/resources/views/users/index.blade.php
+++ src/resources/views/users/index.blade.php
@@ -8,7 +8,11 @@
                 <h3 class="h3">ユーザ情報</h3>
                 <div class="card">
                     <div class="card-body text-center">
-                        <img class="rounded-circle d-inline-block" style="width: 100px; height: 100px;" src="{{ $user->avatar }}" alt="ユーザimg">
+                        @if ($user->avatar)
+                            <img class="rounded-circle d-inline-block" style="width: 100px; height: 100px;" src="{{ $user->avatar }}" alt="ユーザimg">
+                        @else
+                            <img class="rounded-circle d-inline-block" style="width: 100px; height: 100px;" src="{{ asset('images/no-user-img.png') }}" alt="ユーザimg">
+                        @endif
                         <h5 class="h5 mt-2">{{ $user->name }}</h5>
                     </div>
                 </div>
